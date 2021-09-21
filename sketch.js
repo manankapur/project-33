1e8cc2073a1a01bba0f8e199e04173ddc36d64ba
manankapur <76730789+manankapur@users.noreply.github.com>
2021-09-21 18:30:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies =  Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg,ground,gimg;
+
+var ice=[];
+var maxSnow=100;
+
+function preload(){
+  bg=loadImage("snow1.jpg");
+  gimg=loadImage("ground.PNG");
+  
+}
+
+function setup() {
+  createCanvas(1300,600);
+  
+  engine=Engine.create();
+  world= engine.world;
+  
+
+
+ground=createSprite(650,670);
+ground.addImage(gimg);
+ground.scale=3.2;
+ground.velocityX=0;
+
+
+if(frameCount % 275 === 0){
+  for(var i=0; i<maxSnow; i++){
+  ice.push(new Snow(random(0,1350), random(0,50)));
+  }
+  }
+
+
+}
+
+function draw() {
+  background(bg);  
+  Engine.update(engine);
+ 
+
+ 
+
+//add gravity
+
+
+  for(var i = 0;i < maxSnow; i++){
+    ice[i].display();
+    ice[i].changePosition();
+    }    
+    
+
+
+ground.display();
+
+  
+  drawSprites();
+
+}
